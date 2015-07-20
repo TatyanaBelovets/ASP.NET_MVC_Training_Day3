03c2c7879443f89a68a46568bfe413bfd176650c
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-20 03:29:54 +0300

    Added files with changes

---
+++ Task1.Library/Polynomial.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Task1.Library
+{
+    public sealed class Polynomial : IEquatable<Polynomial>, ICloneable
+    {
+        private readonly double[] _coefficients;
+        public double Degree { get { return _coefficients.Length == 0 ? Double.NegativeInfinity : _coefficients.Length - 1; } }
+
+        public Polynomial(params double[] coeffs)
+        {
+            if (coeffs == null)
+            {
+                throw new ArgumentNullException("coeffs", "Coefficients cannot be null");
+            }
+            var end = Array.FindLastIndex(coeffs, d => d != 0); //Finds the index of the last non-zero element
+            _coefficients = new double[end + 1];
+            Array.Copy(coeffs, _coefficients, _coefficients.Length);
+        }
+
+        public double this[int i] //indexer
+        {
+            get
+            {
+                if (i < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Index should be nonnegative");
+                }
+                return i > Degree ? 0 : _coefficients[i];
+            }
+        }
+
+        public Polynomial(Polynomial polynomial) : this(polynomial._coefficients) { }
+
+        public bool Equals(Polynomial other)
+        {
+            return other != null && _coefficients.SequenceEqual(other._coefficients);
+        }
+
+        object ICloneable.Clone()
+        {
+            return Clone();
+        }
+
+        public Polynomial Clone()
+        {
+            return new Polynomial(_coefficients);
+        }
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+            for (int i = _coefficients.Length - 1; i >= 0; i--)
+            {
+                if (i == _coefficients.Length - 1)
+                {
+                    output.Append(String.Format("{0}x^{1}", _coefficients[i], i));
+                }
+                else if (i == 1)
+                {
+                    output.Append(String.Format("{0}{1}x", _coefficients[i] < 0 ? "" : "+", _coefficients[i]));
+                }
+                else if (_coefficients[i] != 0 && i != 0)
+                {
+                    output.Append(String.Format("{0}{1}x^{2}", _coefficients[i] < 0 ? "" : "+", _coefficients[i], i));
+                }
+                else if (i == 0)
+                {
+                    output.Append(String.Format("{0}{1}", _coefficients[i] < 0 ? "" : "+", _coefficients[i]));
+                }
+            }
+            return output.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            const int prime = 17;
+            return (int)_coefficients.Aggregate<double, double>(0, (hash, coeff) => hash * prime + coeff);
+        }
+
+        public override bool Equals(Object other)
+        {
+            var a = other as Polynomial;
+            return a != null && Equals(a);
+        }
+
+        public Polynomial WithCoefficients(params Tuple<int, double>[] pairs)
+        {
+            if (pairs == null)
+            {
+                throw new ArgumentNullException("pairs");
+            }
+            var maxIndex = Math.Max(_coefficients.Length - 1, pairs.Max(t => t.Item1));
+            var copy = new double[maxIndex + 1];
+            Array.Copy(_coefficients, copy, _coefficients.Length);
+            foreach (var tuple in pairs)
+            {
+                if (tuple.Item1 >= 0)
+                {
+                    copy[tuple.Item1] = tuple.Item2;
+                }
+            }
+            return new Polynomial(copy);
+        }
+
+        public static Polynomial operator +(Polynomial left, Polynomial right)
+        {
+            int maxLength = Math.Max(left._coefficients.Length, right._coefficients.Length);
+            int minLength = Math.Min(left._coefficients.Length, right._coefficients.Length);
+            double[] newCoeffs = new double[maxLength];
+            for (int i = 0; i < minLength; i++)
+            {
+                newCoeffs[i] = Math.Round(left._coefficients[i] + right._coefficients[i]);
+            }
+            double[] maxLengthCoeffs = left.Degree > right.Degree ? left._coefficients : right._coefficients;
+            for (int i = minLength; i < maxLength; i++)
+            {
+                newCoeffs[i] = maxLengthCoeffs[i];
+            }
+            return new Polynomial(newCoeffs);
+        }
+
+        public static Polynomial operator +(Polynomial left, double right)
+        {
+            return left + new Polynomial(right);
+        }
+        public static Polynomial operator +(double left, Polynomial right)
+        {
+            return right + left;
+        }
+
+        public static Polynomial operator -(Polynomial left, Polynomial right)
+        {
+            return left + (-right);
+        }
+
+        public static Polynomial operator -(Polynomial left, double right)
+        {
+            return left + new Polynomial(-right);
+        }
+
+        public static Polynomial operator -(Polynomial left)
+        {
+            return new Polynomial(left._coefficients.Select(elem => -elem).ToArray());
+        }
+
+        public static Polynomial operator *(Polynomial left, double right)
+        {
+            return new Polynomial(left._coefficients.Select(elem => right * elem).ToArray());
+        }
+
+        public static Polynomial operator *(double left, Polynomial right)
+        {
+            return right * left;
+        }
+
+        public static Polynomial operator /(Polynomial left, double right)
+        {
+            return left * (1.0 / right);
+        }
+
+        public static bool operator ==(Polynomial left, Polynomial right)
+        {
+            return ((object)left == null) ? (object)right == null : left.Equals(right);
+        }
+
+        public static bool operator !=(Polynomial left, Polynomial right)
+        {
+            return !(left == right);
+        }
+
+        public static Polynomial Add(Polynomial left, Polynomial right)
+        {
+            return left + right;
+        }
+
+        public static Polynomial Add(Polynomial left, double right)
+        {
+            return left + right;
+        }
+
+        public static Polynomial Multiply(Polynomial left, double right)
+        {
+            return left * right;
+        }
+
+        public static Polynomial Subtract(Polynomial left, Polynomial right)
+        {
+            return left - right;
+        }
+
+        public static Polynomial Subtract(Polynomial left, double right)
+        {
+            return left - right;
+        }
+
+        public static Polynomial Divide(Polynomial left, double right)
+        {
+            return left / right;
+        }
+    }
+}
---
+++ Task1.Tests/PolynomialTests.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1.Library;
+
+namespace Task1.Tests
+{
+    [TestClass]
+    public class PolynomialTests
+    {
+        [TestMethod]
+        public void Ctor_GivenSingleNonZeroNumber_ReturnsPolynomialOfDegreeZero()
+        {
+            var poly = new Polynomial(46);
+            Assert.AreEqual(0, poly.Degree);
+        }
+
+        [TestMethod]
+        public void Ctor_GivenZeroArguments_ReturnsPolynomialOfSomeDegree()
+        {
+            var poly = new Polynomial();
+            Assert.AreEqual(Double.NegativeInfinity, poly.Degree);
+        }
+
+        [TestMethod]
+        public void Ctor_GivenMultipleCoefficients_ReturnsPolynomialWithSameCoefficients()
+        {
+            double[] coeffs = { 1, 2, 3, 4 };
+            var poly = new Polynomial(1, 2, 3, 4);
+            for (int i = 0; i < poly.Degree; i++)
+            {
+                Assert.AreEqual(coeffs[i], poly[i]);
+            }
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void Ctor_GivenNull_ThrowsArgumentNullException()
+        {
+            double[] array = null;
+            var poly = new Polynomial(array);
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Indexer_GivenNegativeValue_ThrowsArgumentOutOfRange()
+        {
+            var poly = new Polynomial(1, 5, 2, 9);
+            double _ = poly[-3];
+        }
+
+        [TestMethod]
+        public void Indexer_GivenValuesOverDegree_ReturnsZero()
+        {
+            var poly = new Polynomial(1, 5, 2, 9);
+            Assert.AreEqual(0, poly[30]);
+        }
+
+        [TestMethod]
+        public void Equals_GivenSamePolynomial_ReturnsTrue()
+        {
+            var poly = new Polynomial(5, 8, 5);
+            Assert.IsTrue(poly.Equals(new Polynomial(5, 8, 5)));
+        }
+
+        [TestMethod]
+        public void Equals_GivenNull_ReturnsFalse()
+        {
+            var poly = new Polynomial(5, 8, 5);
+            Assert.IsFalse(poly.Equals(null));
+        }
+
+        [TestMethod]
+        public void Clone_GivenClone_ReturnsTrue()
+        {
+            var poly = new Polynomial(5, 8, 5);
+            var polyClone = poly.Clone();
+            Assert.AreEqual(poly, polyClone);
+        }
+
+        [TestMethod]
+        public void Clone_GivenZeroArgument_ReturnsTrue()
+        {
+            var poly = new Polynomial();
+            var polyClone = poly.Clone();
+            Assert.AreEqual(poly, polyClone);
+        }
+
+        [TestMethod]
+        public void ToString_GivenPolynomialWithPositiveCoeffs_ReturnsString()
+        {
+            var poly = new Polynomial(5, 8, 5);
+            var polyString = "5x^2+8x+5";
+            Assert.AreEqual(polyString, poly.ToString());
+        }
+
+        [TestMethod]
+        public void ToString_GivenPolynomialWithNegativeCoeffs_ReturnsString()
+        {
+            var poly = new Polynomial(-5, -8, -5);
+            var polyString = "-5x^2-8x-5";
+            Assert.AreEqual(polyString, poly.ToString());
+        }
+
+        [TestMethod]
+        public void GetHashCode_GivenEqualsPolynomial_ReturnsTheSamrResult()
+        {
+            var poly1 = new Polynomial(-5, -8, -5);
+            var poly2 = new Polynomial(-5, -8, -5);
+            Assert.AreEqual(poly1.GetHashCode(), poly2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void Add_GivenTwoPolynomials_ReturnsSum()
+        {
+            var result = new Polynomial(3, 5, 2);
+            var poly1 = new Polynomial(2, 7, 9);
+            var poly2 = new Polynomial(1, -2, -7);
+            Assert.AreEqual(result, poly1 + poly2);
+        }
+        [TestMethod]
+        public void Add_GivenTwoPolynomialsNotTheSameDegree_ReturnsSum()
+        {
+            var result = new Polynomial(3, 5, 2, 1);
+            var poly1 = new Polynomial(2, 7, 9, 1);
+            var poly2 = new Polynomial(1, -2, -7);
+            Assert.AreEqual(result, poly1 + poly2);
+        }
+
+        [TestMethod]
+        public void Add_GivenPolynomialAndNumber_ReturnsSum()
+        {
+            var result = new Polynomial(6, 5, 2);
+            var number = 3;
+            var poly = new Polynomial(3, 5, 2);
+            Assert.AreEqual(result, poly + number);
+        }
+
+        [TestMethod]
+        public void Difference_GivenTwoPolynomials_ReturnsDifference()
+        {
+            var result = new Polynomial(3, 5, 2, 1);
+            var poly1 = new Polynomial(2, 7, 9, 1);
+            var poly2 = new Polynomial(1, -2, -7);
+            Assert.AreEqual(poly2, result - poly1);
+        }
+        [TestMethod]
+        public void Difference_GivenTwoPolynomialsNotTheSameDegree_ReturnsDifference()
+        {
+            var result = new Polynomial(3, 5, 2, 1);
+            var poly1 = new Polynomial(2, 7, 9, 1);
+            var poly2 = new Polynomial(1, -2, -7);
+            Assert.AreEqual(poly1, result - poly2);
+        }
+
+        [TestMethod]
+        public void Difference_GivenPolynomialAndNumber_ReturnsDifference()
+        {
+            var result = new Polynomial(6, 5, 2);
+            var number = 3;
+            var poly = new Polynomial(3, 5, 2);
+            Assert.AreEqual(poly, result - number);
+        }
+
+        [TestMethod]
+        public void Multiplication_GivenPolynomialAndNumber_ReturnsMultiplication()
+        {
+            var result = new Polynomial(9, 15, 6);
+            var number = 3;
+            var poly = new Polynomial(3, 5, 2);
+            Assert.AreEqual(result, poly * number);
+        }
+
+        [TestMethod]
+        public void Division_GivenPolynomialAndNumber_ReturnsDivision()
+        {
+            var result = new Polynomial(9, 15, 6);
+            var number = 3;
+            var poly = new Polynomial(3, 5, 2);
+            Assert.AreEqual(poly, result / number);
+        }
+
+        [TestMethod]
+        public void EqualsSign_GivenTwoSamePolynomials_ReturnsTrue()
+        {
+            var poly1 = new Polynomial(3, 5, 2);
+            var poly2 = new Polynomial(3, 5, 2);
+            Assert.IsTrue(poly1 == poly2);
+        }
+
+        [TestMethod]
+        public void EqualsSign_GivenTwoNotTheSamePolynomials_ReturnsFalse()
+        {
+            var poly1 = new Polynomial(3, 5, 2);
+            var poly2 = new Polynomial(3, 4, 2);
+            Assert.IsFalse(poly1 == poly2);
+        }
+
+        [TestMethod]
+        public void NotEqualsSign_GivenTwoSamePolynomials_ReturnsFalse()
+        {
+            var poly1 = new Polynomial(3, 5, 2);
+            var poly2 = new Polynomial(3, 5, 2);
+            Assert.IsFalse(poly1 != poly2);
+        }
+
+        [TestMethod]
+        public void NotEqualsSign_GivenTwoNotTheSamePolynomials_ReturnsTrue()
+        {
+            var poly1 = new Polynomial(3, 4, 2);
+            var poly2 = new Polynomial(3, 5, 2);
+            Assert.IsTrue(poly1 != poly2);
+        }
+    }
+}
